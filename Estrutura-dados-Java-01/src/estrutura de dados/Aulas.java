97bb2c6bd6bad27145130077213c1937b4d995ef
Felipe Selva ☠ <sellvafelipe@gmail.com>
2023-05-19 16:05:52 -0300

    att

--- Estrutura-dados-Java-01/src/estrutura de dados/Aulas.java
+++ Estrutura-dados-Java-01/src/estrutura de dados/Aulas.java
@@ -1,24 +1,97 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package estrutura.dados.java.pkg01;
 
-/**
- *
- * @author Meu Computador
- */
-public class Aula03 {
+import java.util.Arrays;
+
+
+public class Vetor {
     
-    public static void main(String[] args) {
-        Vetor vetor = new Vetor (10);
-        
-        vetor.adicionar("elemento1");
-        vetor.adicionar ("elemento2");
-        vetor.adicionar("elemento3");
-        
-        System.out.println(vetor.busca("elemento1"));
-        
+    private String [] elementos;
+    private int tamanho;
+    public Vetor( int capacidade) {
+        this.elementos = new String[capacidade];
+        this.tamanho = 0;
         
     }
-}
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    public boolean adicionar (String elemento) {
+    
+        /*AULA 01 SOBRE COMO ADICIONAR ELEMENTOS AO VETOR 
+        
+        for ( int i=0; i < elementos.length; i++) {
+           if (this.elementos[i] == null) {
+               this.elementos[i] = elemento;
+               break;}*/
+               
+        
+        //aula 02 tamanho do vetor 
+        
+        if ( tamanho < elementos.length){
+             this.elementos[this.tamanho] = elemento;
+        this.tamanho++;
+        return true;
+           }
+               return false;
+        }
+
+    /*obter elemento de um posicao Aula 03 
+    
+    public String busca ( int posicao) {
+        if (!(posicao >=0 && posicao < tamanho)) {
+            throw new IllegalArgumentException("Posição invalida"); 
+        }
+        return this.elementos[posicao];
+    }*/
+    
+    
+    // aula 04  Verificar se elemento existe no vetor 
+    
+    public int busca (String elemento) {
+        for (int i=0; i < this.tamanho; i++) {
+            if ( this.elementos[i].equals(elemento)) {
+        
+    return i; 
+     
+     }
+    } 
+        
+     return -1;
+       
+    }
+    
+    
+    
+    public int tamanho () {
+        return this.tamanho;
+        
+    }
+
+    @Override
+    public String toString() {
+        
+        
+        
+        return Arrays.toString (elementos);
+    }
+
+    
+    
+    
+    
+   
+       
+            
+        
+        
+    } 
+    
+
+
